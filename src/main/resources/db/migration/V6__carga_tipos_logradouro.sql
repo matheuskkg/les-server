21250afdd7e24f310bd16241b6bb3a0021c2b0ca
matheuskkg <matheus.kkg21@gmail.com>
2025-05-18 19:36:06 -0300

    minor: add migrations tipos

---
+++ src/main/resources/db/migration/V6__carga_tipos_logradouro.sql
@@ -0,0 +1,4 @@
+INSERT INTO tipos_logradouro (tpl_id, tpl_tipo) VALUES (tipos_logradouro_seq.nextval, 'Rua');
+INSERT INTO tipos_logradouro (tpl_id, tpl_tipo) VALUES (tipos_logradouro_seq.nextval, 'Avenida');
+INSERT INTO tipos_logradouro (tpl_id, tpl_tipo) VALUES (tipos_logradouro_seq.nextval, 'Estrada');
+INSERT INTO tipos_logradouro (tpl_id, tpl_tipo) VALUES (tipos_logradouro_seq.nextval, 'Viela');
---
+++ src/main/resources/db/migration/V7__carga_tipos_residencia.sql
@@ -0,0 +1,3 @@
+INSERT INTO tipos_residencia (tpr_id, tpr_tipo) VALUES (tipos_residencia_seq.nextval, 'Casa');
+INSERT INTO tipos_residencia (tpr_id, tpr_tipo) VALUES (tipos_residencia_seq.nextval, 'Apartamento');
+
