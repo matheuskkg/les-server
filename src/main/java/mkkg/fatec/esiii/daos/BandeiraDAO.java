12f0a7cbf8005bef3c2cafe582af76c9c2a2695d
matheuskkg <matheus.kkg21@gmail.com>
2025-05-25 19:00:30 -0300

    feat: endpoint GET Bandeira

---
+++ src/main/java/mkkg/fatec/esiii/daos/BandeiraDAO.java
@@ -0,0 +1,38 @@
+package mkkg.fatec.esiii.daos;
+
+import mkkg.fatec.esiii.domain.EntidadeDominio;
+import mkkg.fatec.esiii.domain.cartao.BandeiraResponseDTO;
+import mkkg.fatec.esiii.repositories.BandeiraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BandeiraDAO implements IDAO {
+
+    @Autowired
+    private BandeiraRepository repository;
+
+    @Override
+    public void salvar(EntidadeDominio entidade) {
+
+    }
+
+    @Override
+    public void alterar(EntidadeDominio entidade) {
+
+    }
+
+    @Override
+    public void excluir(EntidadeDominio entidade) {
+
+    }
+
+    @Override
+    public List<EntidadeDominio> consultar(EntidadeDominio entidade) {
+        List<BandeiraResponseDTO> bandeiras = repository.findAllAsBandeiraResponseDtoList();
+
+        return List.copyOf(bandeiras);
+    }
+}
---
+++ src/main/java/mkkg/fatec/esiii/domain/cartao/BandeiraResponseDTO.java
@@ -0,0 +1,13 @@
+package mkkg.fatec.esiii.domain.cartao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import mkkg.fatec.esiii.domain.EntidadeDominio;
+
+@Getter
+@AllArgsConstructor
+public class BandeiraResponseDTO extends EntidadeDominio {
+
+    private String bandeira;
+
+}
