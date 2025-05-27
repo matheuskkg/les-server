6dec6fc5163692bd6963f95c1477a6f4c2a38131
matheuskkg <matheus.kkg21@gmail.com>
2025-05-27 09:48:02 -0300

    minor: alterar response dos binding results

--- src/main/java/mkkg/fatec/esiii/util/Validacao.java
+++ src/main/java/mkkg/fatec/esiii/util/Validacao.java
@@ -1,16 +1,20 @@
 package mkkg.fatec.esiii.util;
 
+import mkkg.fatec.esiii.domain.FachadaResponseDTO;
 import org.springframework.validation.BindingResult;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 //https://gist.github.com/daniel-shuy/212084f23bf67b357076374377dd7773
 public class Validacao {
-    public static List<String> getErrorMessages(BindingResult bindingResult) {
-        return bindingResult.getAllErrors()
+    public static FachadaResponseDTO getErrorMessages(BindingResult bindingResult) {
+        FachadaResponseDTO fachadaResponseDTO = new FachadaResponseDTO();
+
+        fachadaResponseDTO.setMensagens(bindingResult.getAllErrors()
                 .stream()
                 .map(error -> error.getDefaultMessage())
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
+
+        return fachadaResponseDTO;
     }
 }
