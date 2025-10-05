97283ac09243be5b752a5f4cb7eeba7f19910bea
matheuskkg <matheus.kkg21@gmail.com>
2025-10-05 17:01:01 -0300

    feat: criar FormaPagamento e Cupom

---
+++ src/main/java/fatec/mkkg/server/domain/pagamento/FormaPagamento.java
@@ -0,0 +1,7 @@
+package fatec.mkkg.server.domain.pagamento;
+
+import fatec.mkkg.server.domain.EntidadeDominio;
+
+public class FormaPagamento extends EntidadeDominio {
+
+}
---
+++ src/main/java/fatec/mkkg/server/domain/pagamento/cupom/Cupom.java
@@ -0,0 +1,33 @@
+package fatec.mkkg.server.domain.pagamento.cupom;
+
+import fatec.mkkg.server.domain.cliente.Cliente;
+import fatec.mkkg.server.domain.pagamento.FormaPagamento;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "cupons")
+@Data
+public class Cupom extends FormaPagamento {
+
+	@Id
+	@SequenceGenerator(name = "cupons_seq_gen")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "cps_id")
+	private Integer id;
+
+	@Column(name = "cps_codigo")
+	private String codigo;
+
+	@Column(name = "cps_valor")
+	private Integer valor;
+
+	@ManyToOne
+	@JoinColumn(name = "cps_cli_id", referencedColumnName = "cli_id")
+	private Cliente cliente;
+
+	@ManyToOne
+	@JoinColumn(name = "cps_tcp_id", referencedColumnName = "id")
+	private TipoCupom tipo;
+
+}
