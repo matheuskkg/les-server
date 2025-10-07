e2eac9de910cae26adc4ca7be92f167690419e29
matheuskkg <matheus.kkg21@gmail.com>
2025-10-07 00:15:32 -0300

    feat: migration carga categorias

---
+++ src/main/resources/db/migration/V11__carga_categorias_livros.sql
@@ -0,0 +1,7 @@
+insert into categorias_livros (ctl_id, ctl_nome) values (nextval('categorias_livros_seq'), 'Clássico');
+insert into categorias_livros (ctl_id, ctl_nome) values (nextval('categorias_livros_seq'), 'Ficção');
+insert into categorias_livros (ctl_id, ctl_nome) values (nextval('categorias_livros_seq'), 'Emoções e Sentimentos');
+insert into categorias_livros (ctl_id, ctl_nome) values (nextval('categorias_livros_seq'), 'Infantil');
+insert into categorias_livros (ctl_id, ctl_nome) values (nextval('categorias_livros_seq'), 'Líderes e Pessoas Notáveis');
+insert into categorias_livros (ctl_id, ctl_nome) values (nextval('categorias_livros_seq'), 'Biografias e Histórias Reais');
+insert into categorias_livros (ctl_id, ctl_nome) values (nextval('categorias_livros_seq'), 'Romance');
